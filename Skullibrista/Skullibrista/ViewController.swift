6f9fdd909222224b07e0ac31dec56170412cc5eb
Henrique Alves Batochi <32850493+halvesbatochi@users.noreply.github.com>
2022-05-03 18:03:23 -0300

    Logica do game

--- Skullibrista/Skullibrista/ViewController.swift
+++ Skullibrista/Skullibrista/ViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import CoreMotion
 
 class ViewController: UIViewController {
 
@@ -15,12 +16,86 @@ class ViewController: UIViewController {
     @IBOutlet weak var lbTimePlayed: UILabel!
     @IBOutlet weak var lbInstructions: UILabel!
     
+    var isMoving = false
+    var gameTimer: Timer!
+    var startDate: Date!
+    lazy var motionManager = CMMotionManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         viGameOver.isHidden = true
+        
+        street.frame.size.width = view.frame.size.width * 2
+        street.frame.size.height = street.frame.size.width * 2
+        
+        player.center = view.center
+        player.animationImages = []
+        for i in 0...7 {
+            let image = UIImage(named: "player\(i)")!
+            player.animationImages?.append(image)
+        }
+        player.animationDuration = 0.5
+        player.startAnimating()
+        
+        Timer.scheduledTimer(withTimeInterval: 6.0, repeats: false) { (timer) in
+            self.start()
+        }
+    }
+    
+    func start() {
+        lbInstructions.isHidden = true
+        viGameOver.isHidden = true
+        isMoving = false
+        startDate = Date()
+        
+        self.player.transform = CGAffineTransform(rotationAngle: 0)
+        self.street.transform = CGAffineTransform(rotationAngle: 0)
+        
+        if motionManager.isDeviceMotionAvailable {
+            motionManager.startDeviceMotionUpdates(to: .main) { (data, error) in
+                
+                if error == nil {
+                    if let data = data {
+                        print("x:", data.gravity.x, "y:", data.gravity.y, "z:", data.gravity.z)
+                        let angle = atan2(data.gravity.x, data.gravity.y) - .pi
+                        self.player.transform = CGAffineTransform(rotationAngle: CGFloat(angle))
+                        if !self.isMoving {
+                            self.checkGameOver()
+                        }
+                    }
+                }
+            }
+        }
+        gameTimer = Timer.scheduledTimer(withTimeInterval: 4.0, repeats: true, block: { (timer) in
+            self.rotateWorld()
+        })
+    }
+    
+    func rotateWorld() {
+        let randomAngle = Double(arc4random_uniform(120))/100 - 0.6
+        isMoving = true
+        UIView.animate(withDuration: 0.75, animations: {
+            self.street.transform = CGAffineTransform(rotationAngle: randomAngle)
+        }) {(success) in
+            self.isMoving = false
+        }
+    }
+    
+    func checkGameOver() {
+        let worldAngle = atan2(Double(street.transform.a), Double(street.transform.b))
+        let playerAngle = atan2(Double(player.transform.a), Double(player.transform.b))
+        let difference = abs(worldAngle - playerAngle)
+        if difference > 0.25 {
+            gameTimer.invalidate()
+            viGameOver.isHidden = false
+            motionManager.stopDeviceMotionUpdates()
+            let secondsPlayed = round(Date().timeIntervalSince(startDate))
+            lbTimePlayed.text = "Você jogou durante \(secondsPlayed) segundos"
+        }
     }
 
     @IBAction func playAgain(_ sender: Any) {
+        start()
     }
     
 }
